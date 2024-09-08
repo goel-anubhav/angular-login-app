6583f1f85575cddb70feed6f5605dfd05857dd92
Anubhav Goel <anubhavgoyal12062001@gmail.com>
2024-09-08 13:59:34 +0530

    ng new --no-standalone

---
+++ loginapp/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {MatButtonModule} from "@angular/material/button"
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MatButtonModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
